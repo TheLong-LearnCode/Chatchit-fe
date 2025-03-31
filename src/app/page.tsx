9cf8ad2bf86f1a4775eb3e061fcb161879ac6c59
TheLong-LearnCode <thelong.workhard@gmail.com>
2025-03-31 11:58:42 +0700

    darkmode toggle and signature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,25 @@
 import Footer from "@/components/layout/Footer";
 import ChatBox from "@/components/save/ChatBox";
 import ListChat from "@/components/save/ListChat";
-import { friendsAndGroupChat } from "@/service/api";
-import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import SignatureLoader from "@/components/SignatureLoader";
+import { useState } from "react";
 
 export default function Home() {
   const [selectedConversationId, setSelectedConversationId] = useState();
-  const [isRefreshListChat,setIsRefreshListChat] = useState();
+
   return (
     <div className="flex flex-col h-full">
       <main className="flex flex-1 gap-3">
         <div className="w-1/4 border-r">
-          <ListChat onSelectConversation={setSelectedConversationId}   />
+          <ListChat onSelectConversation={setSelectedConversationId} />
         </div>
         <div className="flex-1 h-full">
           {selectedConversationId ? (
-            <ChatBox conversationId={selectedConversationId}  />
+            <ChatBox conversationId={selectedConversationId} />
           ) : (
-            <p className="text-center mt-10">Chọn một hội thoại để chat</p>
+            <div>
+              <SignatureLoader />
+            </div>
           )}
         </div>
       </main>
--- src/components/toggle-theme.tsx
+++ src/components/toggle-theme.tsx
@@ -1,21 +1,27 @@
 "use client";
 import * as React from "react";
 import { useTheme } from "next-themes";
-import { Switch } from "@/components/ui/switch";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const [isTransitioning, setIsTransitioning] = React.useState(false);
+
   const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setTheme(theme === "light" ? "dark" : "light");
+      setIsTransitioning(false);
+    }, 100);
   };
+
   return (
-    <div className="flex items-center space-x-8">
-      <Switch
-        checked={theme === "dark"}
-        onCheckedChange={toggleTheme}
-        aria-label="Toggle theme"
-        theme={theme}
-      />
+    <div
+      className={`switch-toggle ${
+        theme === "light" ? "lightMode" : "darkMode"
+      } ${isTransitioning ? "transitioning" : ""}`}
+      onClick={toggleTheme}
+    >
+      <div className="toggle-circle"></div>
     </div>
   );
 }
